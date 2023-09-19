7fe2bee05e2b36653f652574f97c1955761e3530
LeLwy <Louis.Chancioux@gmail.com>
2023-09-19 10:38:38 +0200

    Écriture du composant 'Header.js'

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Navigation from './Navigation';
+
+const Header = () => {
+
+    return (
+        <div className='header-container'>
+            <Navigation />
+            <h2>On mate quoi ?</h2>
+        </div>
+    );
+};
+
+export default Header;
