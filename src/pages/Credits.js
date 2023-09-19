d89f64639ba02995f17e94f9e779198e549f3862
LeLwy <Louis.Chancioux@gmail.com>
2023-09-19 10:53:27 +0200

    MaJ 'Credits.js' et 'Favorites.js'

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Credits = () => {
     return (
         <div>
             <h1 className='sr-only'>On mate quoi ? - Crédits</h1>
+            <Header />
         </div>
     );
 };
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Favorites = () => {
     return (
         <div>
             <h1 className='sr-only'>On mate quoi ? - Favoris</h1>
+            <Header />
         </div>
     );
 };
