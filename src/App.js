21431dd98918299f80f828376a98bd39f61d4eec
LeLwy <Louis.Chancioux@gmail.com>
2023-09-19 10:45:39 +0200

    Écriture du composant 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import Credits from "./pages/Credits";
+import NotFound from "./pages/NotFound";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favoris" element={<Favorites />} />
+        <Route path="/credits" element={<Credits />} />
+        {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
